d3de9136a955b3808a5890b7a14ee26aeb46e607
asahiro <asahiro.g@gmail.com>
2017-01-17 05:31:52 +0900

    fix poor bug 2

--- action.php
+++ action.php
@@ -38,7 +38,7 @@ class action_plugin_disableloginform extends DokuWiki_Action_Plugin {
 
     if(empty($form->_content) || !is_array($form->_content)){ return; }
 
-    $startPos = findElementByAttribute('_legend', $lang['btn_login']);// findElementByType('openfieldset');
+    $startPos = $form->findElementByAttribute('_legend', $lang['btn_login']);// $form->findElementByType('openfieldset');
     if($startPos === false){ return; }
 
     addHidden('id', null);
